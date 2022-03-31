d5928d5b3095edbd9addb83439d697aac63eed43
David Edey <david@rdx.works>
2022-03-31 12:54:53 +0100

    Fix generation script

--- regeneration/regen-core-client.sh
+++ regeneration/regen-core-client.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
+
+set -e
+
+# NB - as currently written, this script has only been tested on Mac OSX
+# In particular, Mac makes use of a variant of sed which might not work on other UNIX variants
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR"
+
 java -jar ./openapi-generator-cli-5.2.1.jar \
     generate \
     -i "./api.yaml" \
